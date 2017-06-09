14d8222cd596a4a007373958d5b10f4018d0bcbd
Risto Laanoja <risto.laanoja@guardtime.com>
2017-06-09 14:11:54 +0300

    after linting

--- Formula/ksi-tool.rb
+++ Formula/ksi-tool.rb
@@ -2,15 +2,7 @@ class KsiTool < Formula
   desc "KSI Command-line Tool"
   homepage "https://github.com/guardtime/ksi-tool/"
   url "https://github.com/guardtime/ksi-tool/archive/v2.5.1125.tar.gz"
-  version "v2.5.1125"
   sha256 "679fcd81783f5fdff2786c4a3b75fdd9071c5f9dba2d75da9eed3ece5bd60ccd"
-
-  depends_on "openssl"
-  depends_on "curl"
-  depends_on "libksi"
-  # Release tarball misses configure/make scripts
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
 
   head do
     url "https://github.com/guardtime/ksi-tool.git"
@@ -18,10 +10,15 @@ class KsiTool < Formula
     depends_on "automake" => :build
   end
 
+  depends_on "openssl"
+  depends_on "libksi"
+  # Release tarball misses configure/make scripts
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
 
   def install
-    # system "autoreconf -if" if build.head?
-    system "autoreconf -if"
+    # system "autoreconf", "-if" if build.head?
+    system "autoreconf", "-if"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
--- Formula/libksi.rb
+++ Formula/libksi.rb
@@ -2,15 +2,7 @@ class Libksi < Formula
   desc "KSI C SDK"
   homepage "https://guardtime.github.io/libksi/"
   url "https://github.com/guardtime/libksi/archive/v3.13.2043.tar.gz"
-  version "3.13.2043"
   sha256 "2d51458a5429e6e3d7bec539282a7e58f90b6ef19c63df775f1ec6fa2e5914c8"
-
-  depends_on "openssl"
-  depends_on "curl"
-  # Release tarball misses configure/make scripts
-  depends_on "autoconf" => :build
-  depends_on "automake" => :build
-  depends_on "libtool" => :build
 
   head do
     url "https://github.com/guardtime/libksi.git"
@@ -19,10 +11,16 @@ class Libksi < Formula
     depends_on "libtool" => :build
   end
 
+  depends_on "openssl"
+  # depends_on "curl" # macos-provided is ok
+  # Release tarball misses configure/make scripts
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool" => :build
 
   def install
-    # system "autoreconf -if" if build.head?
-    system "autoreconf -if"
+    # system "autoreconf", "-if" if build.head?
+    system "autoreconf", "-if"
     system "./configure", "--disable-debug",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
